7048dfd3bd11cb4c43b1cf51982bd3ce68e6bc41
Gabriel Russell <gabriel@wiccatech.com>
2013-10-17 12:49:31 -0400

    prettier solution for 3

--- 3.c
+++ 3.c
@@ -3,13 +3,13 @@
 #include "euler.h"
 
 int main(int argc, char ** argv) {
-	unsigned long n = 600851475143;
-	long sn = sqrt(n);
-	long i;
-	for (i = 2; i<sn; i++) {
-		while (n%i==0) {
-			n/=i;
-			sn = sqrt(n);
+	unsigned long n = 600851475143, sn = sqrt(n), i = 2;
+	while ( i < sn ) {
+		if ( n % i ) {
+                    i++;
+                } else {
+		    n /= i;
+		    sn = sqrt( n );
 		}
 	}
         checkanswer(n,6857);
