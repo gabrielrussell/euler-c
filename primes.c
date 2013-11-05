41b35b457a145287cc572fdedde2bbc3946079f4
Gabriel Russell <gabriel@wiccatech.com>
2013-11-05 15:00:52 -0500

    dont check for factors greater then the sqrt in primes.c

--- primes.c
+++ primes.c
@@ -1,18 +1,21 @@
 
 #include <stdint.h>
 #include "dar.h"
+#include <math.h>
 
 void next_prime(uint64_t *n,dar_t *primes) {
     if (*n==0) {
         *n=2;
     } else {
         int is_prime;
-        int i , *p;
+        uint64_t i , *p;
         do {
             (*n)++;
             is_prime = 1;
-            for ( i=0; i<primes->element_count; i++) {
+            uint64_t nsqr = sqrt(*n);
+            for ( i=0; i < primes->element_count; i++) {
                 dar_fetch(primes,i,(void **)&p);
+                if ( *p > nsqr ) break;
                 if ( ( *n % *p ) == 0 ) {
                     is_prime = 0;
                     break;
